d176b7df1ae8f2016bd155495b70d552f797caf8
jadz200 <jadzarzour3@gmail.com>
2022-02-25 19:06:20 +0200

    made the collect static command work

--- DMS_project/settings.py
+++ DMS_project/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1_r-xjm8eo)+#1%uv)4cn%m_dp=38h#7!m$1wmdbeg!k1qs%xi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #changed when hosted in heroku
+DEBUG = False #changed when hosted in heroku
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["klinik-dms-backend.herokuapp.com","127.0.0.1"]
 
 
 # Application definition
@@ -49,6 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -97,6 +98,10 @@ DATABASES = {
 
 }
 
+import dj_database_url 
+db_from_env =dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
@@ -139,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
+STATIC_ROOT= os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = 'static/'
 
 # Default primary key field type
