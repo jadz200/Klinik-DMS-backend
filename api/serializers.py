07059c57bf2382c3f0dffe8e28ed628ebe649e2f
jadz200 <jadzarzour3@gmail.com>
2022-02-28 16:27:23 +0200

    added role crud

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,14 @@
 from dataclasses import field
 from pyexpat import model
+from statistics import mode
 from  rest_framework import serializers
-from .models import Patient
+from .models import Patient, Role
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model= Patient
         fields ='__all__'
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        mode= Role
+        fields='__all__'
--- api/views.py
+++ api/views.py
@@ -3,21 +3,24 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import PatientSerializer
-
-from .models import Patient
+from .serializers import *
+from .models import *
 # Create your views here.
 
-
+#Patient VIEWS
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'List': '/patient/',
-        'Detailed view': '/patient/<str:pk>',
-        'Create': '/patients/',
-        'Update': '/patients/<str:pk>',
-        'Delete': '/patients/<str:pk>',
-        'admin/':'shows the admin panel',
+        'Patient List': '/patient',
+        'Patient Detailed view': '/patient/<str:pk>',
+        'Patient Create': '/patient-create',
+        'Patient Update': '<str:pk>/patient',
+        'Patient Delete': '<str:pk>/patient',
+        'Role List': '/role',
+        'Role Detailed view': '/role/<str:pk>',
+        'Role Create': '/role/',
+        'Role Update': '/<str:pk>/role',
+        'Role Delete': '/<str:pk>/role',
     }
     return Response(api_urls)
 
@@ -65,4 +68,52 @@ def patientUpdate(request, pk):
 def patientDelete(request, pk):
     patient = Patient.objects.get(id=pk)
     patient.delete()
+    return Response('Delete')
+
+#ROLE VIEWS
+
+@api_view(['GET'])
+def roleList(request):
+    role =  Role.objects.all()
+    serializer = RoleSerializer(role, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def roleDetail(request, pk):
+    role = Role.objects.get(id=pk)
+    serializer = RoleSerializer(role, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def roleCreate(request):
+    serializer = RoleSerializer(data=request.data)
+    print(request.data)
+    print(serializer)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def roleUpdate(request, pk):
+    role = Role.objects.get(id=pk)
+    serializer = RoleSerializer(instance=role, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def roleDelete(request, pk):
+    role = Role.objects.get(id=pk)
+    role.delete()
     return Response()
+
