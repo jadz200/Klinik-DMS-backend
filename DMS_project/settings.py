a9966c315e28c63b09543370f79eaf5e1d41bbc2
jadz200 <jadzarzour3@gmail.com>
2022-03-09 12:49:10 +0200

    test

--- DMS_project/settings.py
+++ DMS_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1_r-xjm8eo)+#1%uv)4cn%m_dp=38h#7!m$1wmdbeg!k1qs%xi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #changed when hosted in heroku
 
-ALLOWED_HOSTS = ["klinik-dms-backend.herokuapp.com","127.0.0.1","localhost","klinic-dms.netlify.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -159,8 +159,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+
 
 CORS_ORIGIN_ALLOW_ALL=True
