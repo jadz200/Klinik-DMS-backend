8cc08f0c12e5a3a8f74b4bca09a977e0d392838a
jadz200 <jadzarzour3@gmail.com>
2022-04-22 19:55:12 +0300

    fixing superuser

--- api/models.py
+++ api/models.py
@@ -23,29 +23,26 @@ class Role(models.Model):
 
 class UserManager(UserManager):
   
-    def create_user(self, email,phone,first_name,last_name, password=None,**kwargs):
+    def create_user(self, email, password=None,**kwargs):
         if not email:
             raise ValueError('Email field is required')
 
         email = self.normalize_email(email)
-        user = self.model(email=email,first_name=first_name,last_name=last_name,phone=phone, **kwargs)
+        user = self.model(email=email, **kwargs)
         user.set_password(password)
         user.save()
         
-    def create_staffuser(self, email, password,first_name,last_name,phone):
+    def create_staffuser(self, email, password,):
 
-      user = self.create_user(email,first_name , last_name , phone,password=password )
+      user = self.create_user(email,password=password )
       user.staff = True
       user.save(using=self._db)
       return user
   
-    def create_superuser(self, email, password,first_name,last_name,phone):
+    def create_superuser(self, email, password,):
 
           user = self.create_user(
               email,
-              first_name,
-              last_name,
-              phone,
               password=password,
           )
           user.staff = True
@@ -72,7 +69,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     created_at = models.DateTimeField(auto_now_add=True)
     last_login= models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name','last_name','phone']
+    REQUIRED_FIELDS = ['username']
 
 
     def get_full_name(self):
